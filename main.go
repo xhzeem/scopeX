bd24ccac50723f9834557bb3c5fddff85a402cb4
أنس روبي <34074156+xhzeem@users.noreply.github.com>
2023-01-08 08:22:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"regexp"
-	"strings"
+	"encoding/json"
 )
 
 type ExObject struct {
@@ -18,61 +17,62 @@ type ExObject struct {
 
 func main() {
 
-	var inputJSONString string
-	flag.StringVar(&inputJSONString, "f", "", "input JSON file for execlude object")
+	var excludeObj string
+	flag.StringVar(&excludeObj, "x", "", "Exclude JSON object")
 
 	var filterMode int
-	flag.IntVar(&filterMode, "mode", 0, "filter mode from 1-4")
+	flag.IntVar(&filterMode, "m", 0, "Filter mode from 1-4")
 
 	flag.Parse()
 
 	// Unmarshal the JSON data into a JSONObject
 	var inputJSON ExObject
-	if err := json.Unmarshal([]byte(inputJSONString), &inputJSON); err != nil {
+	if err := json.Unmarshal([]byte(excludeObj), &inputJSON); err != nil {
 		fmt.Println("Error parsing the JSON object:", err)
 		os.Exit(1)
 	}
 
-	// Read the list of URLs from stdin
-	inputSubs, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		fmt.Println("Error reading subdomains from stdin:", err)
-		os.Exit(1)
-	}
+	// read URLs on stdin, and print if not excluded
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
 
-	// Split the list of URLs by newline
-	subList := strings.Split(string(inputSubs), "\n")
+		switch filterMode {
+			case 1: 	// Subdmoains Mode
+				iSub := sc.Text()
+				matched := false
+				// Iterate over the list of regexes in inputJSON.Sub
+				for _, fSub := range inputJSON.Sub {
+					fSubRegex, err := regexp.Compile("^" + fSub + "$")
+					if err != nil {
+						fmt.Println("Error compiling regex:", err)
+						os.Exit(1)
+					}
 
-	if filterMode == 0 {
-		fmt.Fprintln(os.Stderr, "--mode 1: filter by subdmoains, input [sub]")
-		fmt.Fprintln(os.Stderr, "--mode 2: filter by IP address, input [sub, ip]")
-		fmt.Fprintln(os.Stderr, "--mode 3: filter by cname, input: [sub, cname]")
-	} else if filterMode == 1 {
-
-		// Iterate over the list of URLs
-		for _, iSub := range subList {
-			// Set matched to false initially
-			matched := false
-
-			// Iterate over the list of regexes in inputJSON.Sub
-			for _, fSub := range inputJSON.Sub {
-				fSubRegex, err := regexp.Compile(fSub+"$")
-				if err != nil {
-					fmt.Println("Error compiling regex:", err)
-					os.Exit(1)
+					// Check if the Sub matches the fSub regex
+					if fSubRegex.MatchString(iSub) {
+						matched = true
+						break
+					}
 				}
 
-				// Check if the iSub matches the fSub regex
-				if fSubRegex.MatchString(iSub) {
-					// If it does, set matched to true and break out of the loop
-					matched = true
-					break
+				if !matched {
+					fmt.Println(iSub)
 				}
-			}
+			
+			case 2: 	// IP Mode
+				fmt.Println("IP Mode")
+			
+			case 3: 	// CNAME Mode
+				fmt.Println("CNAME Mode")
 
-			if !matched {
-				fmt.Println(iSub)
-			}
+			case 0:
+				fallthrough
+			default:
+				fmt.Fprintln(os.Stderr, "--mode 1: filter by subdmoains, input [sub]")
+				fmt.Fprintln(os.Stderr, "--mode 2: filter by IP address, input [sub, ip]")
+				fmt.Fprintln(os.Stderr, "--mode 3: filter by cname, input: [sub, cname]")
+				os.Exit(1)
 		}
+
 	}
 }
